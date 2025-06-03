2df480e6baaff37cf1345bdae87e98f8615ab961
Emma Ogwayo <emmaogwayo@gmail.com>
2025-06-03 10:46:29 +0300

    Emma Added Interface Segregation Principle: adhering and breaking code examples

--- Interface_Segregation_Principle/Adhering_Code/BasicPrinter.java
+++ Interface_Segregation_Principle/Adhering_Code/BasicPrinter.java
@@ -1,25 +1,9 @@
 package Interface_Segregation_Principle.Adhering_Code;
 
-interface Printer {
-    void print();
-}
-
-interface Scanner {
-    void scan();
-}
-
 class BasicPrinter implements Printer {
     public void print() {
         System.out.println("Printing...");
     }
 }
 
-class AllInOnePrinter implements Printer, Scanner {
-    public void print() {
-        System.out.println("Printing from all-in-one...");
-    }
 
-    public void scan() {
-        System.out.println("Scanning...");
-    }
-}
